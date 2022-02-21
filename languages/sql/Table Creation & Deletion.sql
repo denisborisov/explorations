c8e7fbae721d4bac39f2e39d6ce957998d8bc422
Denis Borisov <denis_borisov@epam.com>
2022-02-21 19:08:35 +0300

    Added info about table creation & deletion

---
+++ languages/sql/Table Creation & Deletion.sql
@@ -0,0 +1,27 @@
+/* DB2 for i */
+
+create or replace table users
+(
+    id integer
+)
+drop table if exists users
+
+
+
+/* MySQL */
+
+create table if not exists users
+(
+    id int
+);
+drop table if exists users;
+
+
+
+/* PostgreSQL */
+
+create table if not exists users
+(
+    id integer
+);
+drop table if exists users;
