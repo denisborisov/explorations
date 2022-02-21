92262554f5de9e1e8460ac1a38699a4321be0cf8
Denis Borisov <denis_borisov@epam.com>
2022-02-21 16:38:25 +0300

    Added info about database creation & deletion

---
+++ languages/sql/Database Creation & Deletion.sql
@@ -0,0 +1,21 @@
+/*
+DB2 for i
+[Windows] 5250 Emulator provided by IBM i Access Client Solutions.
+
+* crtlib lib({{library}})
+    * e.g. crtlib lib(DBLIB)
+* dltlib lib({{library}})
+    * e.g. dltlib lib(DBLIB)
+*/
+
+
+/* MySQL */
+
+create database a_database;
+drop database a_database;
+
+
+/* PostgreSQL */
+
+create database a_database;
+drop database a_database;
