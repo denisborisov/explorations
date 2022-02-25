1ce7fdf4b3a9dcc7775e585c32b30c3a466fca6f
Denis Borisov <denis_borisov@epam.com>
2022-02-25 20:09:14 +0300

    Excluded unnecessary info

--- technologies/databases/sql/syntax/4. Data types. Enumerations.sql
+++ technologies/databases/sql/syntax/4. Data types. Enumerations.sql
@@ -7,10 +7,6 @@
 --
 --
 -- MySQL
---   Inserting 1 million rows into this table with a value of 'medium'
---   would require 1 million bytes of storage, as opposed to 6 million bytes
---   if you stored the actual string 'medium' in a VARCHAR column.
-drop table if exists tshirts;
 create table if not exists tshirts
 (
     size enum('small', 'medium', 'large') 
@@ -19,7 +15,6 @@ create table if not exists tshirts
 --
 -- PostgreSQL
 create type tsize as enum ('small', 'medium', 'large');
-drop table if exists tshirts;
 create table if not exists tshirts
 (
 	size tsize
